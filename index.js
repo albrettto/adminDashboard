466e0b6c5270de6e166eb7aeb4ef23421bb18f3b
albrettto <a.husamov@yandex.ru>
2024-01-12 02:17:51 +0500

    Final Result

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkmode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+darkmode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkmode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkmode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
 Orders.forEach(order =>{
     const tr = document.createElement('tr');
     const trContent = `
